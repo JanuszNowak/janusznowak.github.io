c185ff0734c008fb4a87a46b7833edd7f134a421
Janusz Nowak <janono@gmail.com>
2022-06-11 20:19:59 +0200

    Add cover script for converting to webp

---
+++ conver.ps1
@@ -0,0 +1,26 @@
+$libPath = "C:\Users\jnno\Downloads\libwebp-1.2.2-windows-x64\bin\cwebp.exe"
+$folder = "\\wsl.localhost\Ubuntu\home\jnno\janusznowak.github.io"
+$files = get-childitem $folder -recurse -force -include *.jpg
+
+$files|Out-GridView
+foreach ($inputFile in $files) {
+
+   #Call cwebp
+   $inputFilePath = $inputFile.FullName
+   $outputFilePath = Split-Path -Path $inputFile.FullName
+   $outputFilePath = $outputFilePath + "\" + [System.IO.Path]::GetFileNameWithoutExtension($inputFilePath) + ".webp"
+
+   $args = "-q 80 " + $inputFilePath + " -o " + $outputFilePath
+
+   Start-Process -FilePath $libPath -ArgumentList $args -Wait
+
+   $originalFileSize = (Get-Item $inputFilePath).length
+   $optimizedFileSize = (Get-Item $outputFilePath).length
+
+
+   #Prepare output
+   $savedBytes = $originalFileSize - $optimizedFileSize
+   $savedPercentage = [math]::Round(100 - ($optimizedFileSize / $originalFileSize) * 100)
+   $message = $inputFilePath + " " + $outputFilePath + " " + $savedBytes + "bytes " + $savedPercentage + "%"
+   Write-Output $message
+}
